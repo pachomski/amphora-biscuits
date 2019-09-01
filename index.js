e5ad29b6d0972266cb927fa76b4ede01601621d2
Christopher Pachomski <christopher.pachomski@turner.com>
2019-09-01 01:07:00 -0400

    create new state more similar to amphora-html for easier setup with kiln-storybook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const render = require("./lib/render");
-const setup = require("./lib/setup");
+const { render, addEnvVars } = require('./lib/render');
+const setup = require('./lib/setup');
 
 setup();
 
 module.exports.render = render;
+module.exports.addEnvVars = addEnvVars;
