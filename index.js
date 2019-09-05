d5a38ae954ef07a19279a29971faca01b5fd75c2
Christopher Pachomski <christopher.pachomski@turner.com>
2019-09-05 13:29:13 -0400

    create and export setup.addHelpers function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { render, addEnvVars } = require('./lib/render');
-const { setup, addResolveMedia } = require('./lib/setup');
+const { setup, addResolveMedia, addHbsHelpers } = require('./lib/setup');
 
 setup();
 
 module.exports.render = render;
 module.exports.addEnvVars = addEnvVars;
 module.exports.addResolveMedia = addResolveMedia;
+module.exports.addHelpers = addHbsHelpers;
